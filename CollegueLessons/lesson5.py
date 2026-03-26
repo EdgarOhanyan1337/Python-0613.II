6afe0d9208e68f8ee95d18f8f2764b5a4b2c50a2
EdgarOhanyan1337 <junglestandoff@gmail.com>
2026-03-26 09:57:20 +0300

    lesson5&6

---
+++ CollegueLessons/lesson5.py
@@ -0,0 +1,9 @@
+c = 1
+i = 0
+
+for c in range(1,20, 2):
+    print(c)
+    i+=1
+else:
+    print(f"verji tivy {c}")
+    print(f"tvveri qanaky ``{i}")
---
+++ CollegueLessons/lesson6.py
@@ -0,0 +1,10 @@
+#grel cragir vory trvac mijakayqum trvac qaylov khashvi ev kartaci funckcayi arjeqy
+import math
+sum = 0
+x = 7.5
+i = 0.2
+
+while x<12.5:
+    sum = (x+1)**2
+    print(f"your number is {sum}")
+    x+=i
